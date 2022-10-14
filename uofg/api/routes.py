5a3b86054e5d01cfbc884843322f1fa704aadc9b
ywb20140 <fabian.salge.2020@uni.strath.ac.uk>
2022-10-14 20:38:36 +0100

    feat:added method

--- uofg/api/routes.py
+++ uofg/api/routes.py
@@ -1,7 +1,8 @@
+from crypt import methods
 from flask import Blueprint, jsonify
 
 api = Blueprint('main', __name__)
 
-@api.route("/")
+@api.route("/", methods=['GET'])
 def index():
     return jsonify("angus is a goofy")
