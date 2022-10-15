63a42d417252ee3eeb3849b98e87823cb71e04dc
Angus Logan <angus.logan@baronsview.com>
2022-10-15 11:40:54 +0100

    flask_cors

--- uofg/__init__.py
+++ uofg/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from uofg.config import Config
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(Config)
 
     from uofg.api.routes import api
